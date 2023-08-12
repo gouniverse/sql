84f53cc1edbbf049932e24362787e886245aa66e
Milan Lesichkov <lesichkovm@gmail.com>
2023-08-12 09:29:59 +0100

    Update consts.go

--- consts.go
+++ consts.go
@@ -1,11 +1,20 @@
 package sql
 
-const DIALECT_SQLITE = "sqlite"
 const DIALECT_MYSQL = "mysql"
 const DIALECT_POSTGRES = "postgres"
+const DIALECT_SQLITE = "sqlite"
 
-const COLUMN_ATTRIBUTE_LENGTH = "length"
-const COLUMN_ATTRIBUTE_DECIMALS = "decimals"
 const COLUMN_ATTRIBUTE_AUTO = "auto"
-const COLUMN_ATTRIBUTE_PRIMARY = "primary"
+const COLUMN_ATTRIBUTE_DECIMALS = "decimals"
+const COLUMN_ATTRIBUTE_LENGTH = "length"
 const COLUMN_ATTRIBUTE_NULLABLE = "nullable"
+const COLUMN_ATTRIBUTE_PRIMARY = "primary"
+const COLUMN_TYPE_STRING = "string"
+
+const COLUMN_TYPE_STRING = "blob"
+const COLUMN_TYPE_STRING = "date"
+const COLUMN_TYPE_STRING = "datetime"
+const COLUMN_TYPE_STRING = "decimal"
+const COLUMN_TYPE_STRING = "float"
+const COLUMN_TYPE_STRING = "integer"
+const COLUMN_TYPE_STRING = "text"
